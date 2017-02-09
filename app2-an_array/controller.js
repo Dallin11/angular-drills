2da03bebb92fbc535898c0d1b6213bbadd3f44cc
Missy Beutler <missyjeanbeutler@users.noreply.github.com>
2017-02-09 14:26:26 -0700

    finished 10:39

---
+++ app2-an_array/controller.js
@@ -0,0 +1,11 @@
+angular.module('app').controller('ctrl', function($scope, svc){
+
+
+
+
+$scope.getData = function() {
+    $scope.data = svc.getData();
+}
+$scope.getData();
+    
+})
---
+++ app4-api/controller.js
@@ -0,0 +1,27 @@
+angular.module('app').controller('ctrl', function($scope, svc){
+
+
+
+$scope.num = 1;
+
+$scope.getData = function(){
+    svc.getData($scope.num).then(function(response){
+        $scope.data = response;
+    })
+
+
+}
+
+$scope.getData();
+
+
+$scope.changeNum = function() {
+    $scope.getData($scope.num);
+    
+}
+
+
+
+
+    
+})
